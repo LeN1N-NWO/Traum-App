8bea83cbf0ab289f91f3f8636928dbc867d547db
Anton <phantonhouse@gmail.com>
2026-08-07 14:08:51 +0200

    feat: Design-Tokens und Grundlayout als eigene Dateien

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/tokens.css";
+import "./styles/base.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
